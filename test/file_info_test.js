20d9e4c77d42e2b6b6faf9d908d8600c6b3776ac
Hans Meyer <git@hansmeyer.com>
2013-12-26 22:27:25 +0000

    resolve order of function definitions to appease jshint

--- test/file_info_test.js
+++ test/file_info_test.js
@@ -23,15 +23,42 @@ var grunt = require('grunt');
 */
 
 // source: http://stackoverflow.com/a/6969486/384062
-var escapeRegExp = function(iStr) {
-  return iStr.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-};
+
+function escapeRegExp(iStr) {
+	return iStr.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+function addComparisonTest(obj, name, filenames, type, desc) {
+	type = type || 'contains';
+
+	obj[name] = function(test) {
+		test.expect(filenames.length);
+
+		var actual, expected;
+		filenames.forEach(function(el) {
+			actual = grunt.file.read('test/actual/' + el);
+			expected = grunt.file.read('test/expected/' + el);
+
+			if (type === 'equals') {
+				test.equal(actual, expected, desc);
+			} else /*if (type === 'contains')*/ {
+				test.ok((new RegExp(escapeRegExp(expected))).test(actual), desc);
+			}
+		});
+
+		test.done();
+	};
+}
+
+function addStdoutComparisonTest(obj, name, type, desc) {
+	return addComparisonTest(obj, name, ['stdout_' + name + '.txt'], type, desc);
+}
 
 var file_info = {
-  setUp: function(done) {
-    // setup here if necessary
-    done();
-  }
+	setUp: function(done) {
+		// setup here if necessary
+		done();
+	}
 };
 
 addStdoutComparisonTest(file_info, 'default_options');
@@ -54,32 +81,5 @@ addComparisonTest(file_info, 'inject_new_dest', ['inject_dest_1.txt']);
 addComparisonTest(file_info, 'inject_overlay', ['inject_dest_1.txt']);
 addComparisonTest(file_info, 'inject_multiple_dest_1', ['inject_dest_1.txt', 'inject_dest_2.txt', 'inject_dest_3.txt']);
 addComparisonTest(file_info, 'inject_multiple_dest_2', ['inject_dest_1.txt', 'inject_dest_2.txt', 'inject_dest_3.txt']);
-
-
-function addStdoutComparisonTest(obj, name, type, desc) {
-  return addComparisonTest(obj, name, ['stdout_' + name + '.txt'], type, desc);
-}
-
-function addComparisonTest(obj, name, filenames, type, desc) {
-  type = type || 'contains';
-
-  obj[name] = function(test) {
-    test.expect(filenames.length);
-
-    var actual, expected;
-    filenames.forEach(function(el) {
-      actual = grunt.file.read('test/actual/' + el);
-      expected = grunt.file.read('test/expected/' + el);
-
-      if (type === 'equals') {
-        test.equal(actual, expected, desc);
-      } else /*if (type === 'contains')*/ {
-        test.ok((new RegExp(escapeRegExp(expected))).test(actual), desc);
-      }
-    });
-
-    test.done();
-  };
-}
 
 exports.file_info = file_info;
