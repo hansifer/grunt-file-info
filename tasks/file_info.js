7ae4fd46967ebfaafa82653c047e7378e8eb6fb4
Hans Meyer <git@hansmeyer.com>
2013-12-26 16:23:42 +0000

    testing checkin description for 's and "s

--- tasks/file_info.js
+++ tasks/file_info.js
@@ -8,7 +8,7 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function(grunt) { 
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
